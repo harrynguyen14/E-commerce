6ad3439835fc986bb87833b9ee64c80c10f8a0b9
Mivenz <nguyennhatminh11142002@gmail.com>
2025-10-27 11:03:27 +0700

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import ProductList from "@/components/ProductList";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="">
+      {/* <div className="relative aspect-3/1 mb-12">
+      </div> */}
+      <ProductList/>
+    </div>
   );
 }
