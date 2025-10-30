be249ab90317999b29a70683e85a5d2c8fe354b1
Mivenz <nguyennhatminh11142002@gmail.com>
2025-10-30 14:28:42 +0700

    update

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,79 +1,88 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 import {
-    Footprints,
-    Glasses,
-    Briefcase,
-    Shirt,
-    ShoppingBasket,
-    Hand,
-    Venus,
-  } from "lucide-react";
-  import { usePathname, useRouter, useSearchParams } from "next/navigation";
-  
-  const categories = [
-    {
-      name: "All",
-      icon: <ShoppingBasket className="w-4 h-4" />,
-      slug: "all",
-    },
-    {
-      name: "T-shirts",
-      icon: <Shirt className="w-4 h-4" />,
-      slug: "t-shirts",
-    },
-    {
-      name: "Shoes",
-      icon: <Footprints className="w-4 h-4" />,
-      slug: "shoes",
-    },
-    {
-      name: "Accessories",
-      icon: <Glasses className="w-4 h-4" />,
-      slug: "accessories",
-    },
-    {
-      name: "Bags",
-      icon: <Briefcase className="w-4 h-4" />,
-      slug: "bags",
-    },
-    {
-      name: "Dresses",
-      icon: <Venus className="w-4 h-4" />,
-      slug: "dresses",
-    },
-    {
-      name: "Jackets",
-      icon: <Shirt className="w-4 h-4" />,
-      slug: "jackets",
-    },
-    {
-      name: "Gloves",
-      icon: <Hand className="w-4 h-4" />,
-      slug: "gloves",
-    },
-  ];
-  
+  Footprints,
+  Glasses,
+  Briefcase,
+  Shirt,
+  ShoppingBasket,
+  Hand,
+  Venus,
+} from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+const categories = [
+  {
+    name: "All",
+    icon: <ShoppingBasket className="w-4 h-4" />,
+    slug: "all",
+  },
+  {
+    name: "T-shirts",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "t-shirts",
+  },
+  {
+    name: "Shoes",
+    icon: <Footprints className="w-4 h-4" />,
+    slug: "shoes",
+  },
+  {
+    name: "Accessories",
+    icon: <Glasses className="w-4 h-4" />,
+    slug: "accessories",
+  },
+  {
+    name: "Bags",
+    icon: <Briefcase className="w-4 h-4" />,
+    slug: "bags",
+  },
+  {
+    name: "Dresses",
+    icon: <Venus className="w-4 h-4" />,
+    slug: "dresses",
+  },
+  {
+    name: "Jackets",
+    icon: <Shirt className="w-4 h-4" />,
+    slug: "jackets",
+  },
+  {
+    name: "Gloves",
+    icon: <Hand className="w-4 h-4" />,
+    slug: "gloves",
+  },
+];
+
 const Categories = () => {
-    const SearchParams = useSearchParams()
-    const router = useRouter()
+  const SearchParams = useSearchParams();
+  const router = useRouter();
 
-    const selectedCategory = SearchParams.get("category")
-    const handleChange = (value : string | null) => {
-        router.push('/?category=${value')
-    }
-    
-    return (
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm justify-items-center mx-auto max-w-5xl'>
-            {categories.map(categoriy => (
-                <div className={'flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${category.slug === selectedCategory ? "bg-white" : "text-gray-500"}'} key={category.name} onClick={() => handelChange(category.slug)}>
-                    {categoriy.icon}
-                    {categoriy.name}
-                </div>
-            ))}
+  const selectedCategory = SearchParams.get("category");
+  console.log("Select Change", selectedCategory)
+  const handleChange = (value: string | null) => {
+    router.push(`/?category=${value}`);
+  };
+
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-8 gap-2 bg-gray-100 p-2 rounded-lg mb-4 text-sm justify-items-center mx-auto max-w-5xl">
+      {categories.map((category) => (
+        <div
+        className={
+          `flex items-center justify-center gap-2 cursor-pointer px-2 py-1 rounded-md ${
+            category.slug === selectedCategory ? "bg-white" : "text-gray-500"
+          }`
+        }
+          key={category.name}
+          onClick={() => handleChange(category.slug)}
+        >
+          {category.icon}
+          {category.name}
         </div>
-    )
-}
+      ))}
+    </div>
+  );
+};
 
-export default Categories
+export default Categories;
